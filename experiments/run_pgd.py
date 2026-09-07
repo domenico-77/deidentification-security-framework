dd0a2966c79dd551190f055c34de80ebc61994a3
Domenico Vicenti <79905785+domenico-77@users.noreply.github.com>
2026-09-07 12:51:56 +0200

    Update run_pgd.py

--- experiments/run_pgd.py
+++ experiments/run_pgd.py
@@ -1,3 +1,14 @@
+import sys
+from pathlib import Path
+
+# Aggiunge la radice del framework al sys.path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+import argparse
+import yaml
+import torch
+from datasets.lfw import LFWDataset
+
 import argparse
 import yaml
 import torch
