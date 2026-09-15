0d04e5393d71a4f05a29cfd84b0869f71de9cf77
Domenico Vicenti <79905785+domenico-77@users.noreply.github.com>
2026-09-15 12:48:44 +0200

    Update run_pgd.py

--- experiments/run_pgd.py
+++ experiments/run_pgd.py
@@ -51,7 +51,7 @@ def main():
     dataset = LFWDataset(root_dir=args.dataset_path)
     runner = BenchmarkRunner(target, attack, dataset)
 
-    print("Avvio del Benchmark PGD...")
+    #print("Avvio del Benchmark PGD...")
     output_dir = "./results"
     
     # 1. Esecuzione del benchmark (con log in tempo reale grazie al nuovo logger)
