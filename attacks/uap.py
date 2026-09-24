46200587386acdbf665d60788193323389371055
Domenico Vicenti <79905785+domenico-77@users.noreply.github.com>
2026-09-24 14:41:34 +0200

    Update uap.py

--- attacks/uap.py
+++ attacks/uap.py
@@ -14,7 +14,7 @@ class UAPAttack(BaseAttack):
         self.mean_tensor = mean_tensor
         self.uap_perturbation = None
 
-    def fit(self, dataloader, epsilon=16.0, alpha=2, epochs=8, max_iter_per_img=20):
+    def fit(self, dataloader, epsilon=16.0, alpha=2, epochs=8, max_iter_per_img=15):
         self.dsfd_net.eval()
         
         sample_batch = next(iter(dataloader))
